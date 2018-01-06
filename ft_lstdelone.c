d01ad0f517b01946a19c28a0ebb8f10063c729a5
simdax <simoncornaz@gmail.com>
2018-01-06 23:12:07 +0100

    lst change

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,7 +6,7 @@
 /*   By: scornaz <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/10 17:23:00 by scornaz           #+#    #+#             */
-/*   Updated: 2017/11/11 12:42:59 by scornaz          ###   ########.fr       */
+/*   Updated: 2018/01/06 18:58:53 by simdax           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,8 @@
 
 void	ft_lstdelone(t_list **alst, void (*del)(void*, size_t))
 {
-	del((*alst)->content, (*alst)->content_size);
-	free(*alst);
-	*alst = NULL;
+  if (*alst)
+    del((*alst)->content, (*alst)->content_size);
+  free(*alst);
+  *alst = NULL;
 }
