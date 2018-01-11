fb7ac571f20c4333fa7c2d6eb30219f82f92b097
Simon CORNAZ <scornaz@e1r10p16.42.fr>
2018-01-11 19:52:55 +0100

    add opt flags avec getmax

--- list/ft_lstgetmax.c
+++ list/ft_lstgetmax.c
@@ -6,18 +6,19 @@
 /*   By: simdax </var/spool/mail/simdax>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/08 17:45:36 by simdax            #+#    #+#             */
-/*   Updated: 2018/01/08 17:58:00 by simdax           ###   ########.fr       */
+/*   Updated: 2018/01/11 19:41:34 by scornaz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes.h"
 
 int	ft_lstgetmax(t_list *list, int val,
-                      int (*f)(t_list *el, int val))
+				 int (*f)(t_list *el, int val, void *f),
+				 void *opt)
 {
 	while (list)
 	{
-          val = f(list, val);
+		val = f(list, val, opt);
 		list = list->next;
 	}
         return (val);
--- list/includes.h
+++ list/includes.h
@@ -6,7 +6,7 @@
 /*   By: simdax </var/spool/mail/simdax>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/08 14:25:48 by simdax            #+#    #+#             */
-/*   Updated: 2018/01/11 10:03:45 by simdax           ###   ########.fr       */
+/*   Updated: 2018/01/11 19:44:36 by scornaz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,8 @@
 
 int	ft_cmp_rstr(t_list *one, t_list *two);
 int	ft_lstgetmax(t_list *list, int val,
-                      int (*f)(t_list *el, int val));
+				 int (*f)(t_list *el, int val, void *opt),
+				 void *opt);
 t_list	*ft_lstsort(t_list *list, int (*cmp)(t_list*, t_list*));
 int	ft_cmp_strlen(t_list *one, t_list *two);
 int	ft_cmp_str(t_list *one, t_list *two);
