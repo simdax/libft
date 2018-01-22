c1175633e02f738170bfab5c3d321c717bdcb42a
Simon CORNAZ <scornaz@e2r2p14.42.fr>
2018-01-22 14:40:57 +0100

    finish array

--- array/array.h
+++ array/array.h
@@ -6,7 +6,7 @@
 /*   By: scornaz <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/22 11:27:52 by scornaz           #+#    #+#             */
-/*   Updated: 2018/01/22 13:06:15 by scornaz          ###   ########.fr       */
+/*   Updated: 2018/01/22 14:40:46 by scornaz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,14 @@
 typedef struct	s_array
 {
 	void		*mem;
-	size_t		size_t;
+	size_t		type_len;
 	unsigned	space;
 	unsigned	cursor;
 	int			size;
-	int			left;
 }				t_array;
 
-t_array		*new_array();
-void		free_array(t_array *array);
-void		array_add(t_array *array, void *val, int len);
+t_array			*new_array();
+void			free_array(t_array *array);
+void			array_add(t_array *array, void *val, unsigned len);
 
 #endif
