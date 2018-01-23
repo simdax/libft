61a6ecc28b41c1e79fba76c42cc37d5df88714ae
Simon CORNAZ <scornaz@e2r12p5.42.fr>
2018-01-23 12:13:41 +0100

    better unicode

--- unicode/unicode.c
+++ unicode/unicode.c
@@ -6,30 +6,33 @@
 /*   By: scornaz <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/23 10:48:48 by scornaz           #+#    #+#             */
-/*   Updated: 2018/01/23 11:46:57 by scornaz          ###   ########.fr       */
+/*   Updated: 2018/01/23 12:13:05 by scornaz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <unistd.h>
 
-static void		code_to_utf8(unsigned char *const buffer,
+static int		code_to_utf8(unsigned char *const buffer,
 							const unsigned int code)
 {
 	if (code <= 0x7F)
 	{
 		buffer[0] = code;
+		return (1);
 	}
 	else if (code <= 0x7FF)
 	{
 		buffer[0] = 0xC0 | (code >> 6);
 		buffer[1] = 0x80 | (code & 0x3F);
+		return (2);
 	}
 	else if (code <= 0xFFFF)
 	{
 		buffer[0] = 0xE0 | (code >> 12);
 		buffer[1] = 0x80 | ((code >> 6) & 0x3F);
 		buffer[2] = 0x80 | (code & 0x3F);
+		return (3);
 	}
 	else if (code <= 0x10FFFF)
 	{
@@ -37,7 +40,9 @@ static void		code_to_utf8(unsigned char *const buffer,
 		buffer[1] = 0x80 | ((code >> 12) & 0x3F);
 		buffer[2] = 0x80 | ((code >> 6) & 0x3F);
 		buffer[3] = 0x80 | (code & 0x3F);
+		return (4);
 	}
+	return (0);
 }
 
 unsigned char	*unicode_to_utf8(int str)
@@ -70,4 +75,32 @@ void			ft_putstr_utf8(int *str)
 {
 	while (*str)
 		ft_putchar_utf8(*str++);
+}
+
+char			*transform_utf8(int *str)
+{
+	int				nb_char;
+	int				i;
+	unsigned char	*unicode;
+	char			*ret;
+	char			*cpy;
+
+	nb_char = 0;
+	unicode = malloc(5);
+	ft_bzero(unicode, 5);
+	while (str[nb_char])
+		++nb_char;
+	if(!(ret = (char*)malloc(sizeof(int) * nb_char + 1 )))
+		return (0);
+	cpy = ret;
+	while(*str)
+	{
+		i = 0;
+		nb_char = code_to_utf8(unicode, *str++);
+		while (i < nb_char)
+			*ret++ = unicode[i++];
+	}
+	*ret = 0;
+	free(unicode);
+	return (cpy);
 }
--- unicode/unicode.h
+++ unicode/unicode.h
@@ -6,7 +6,7 @@
 /*   By: scornaz <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/23 11:46:23 by scornaz           #+#    #+#             */
-/*   Updated: 2018/01/23 11:46:27 by scornaz          ###   ########.fr       */
+/*   Updated: 2018/01/23 12:13:04 by scornaz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,8 @@
 #define UNICODE_H
 
 unsigned char	*unicode_to_utf8(int str);
+char			*transform_utf8(int *str);
+void			ft_putstr_utf8(int *str);
+int				ft_putchar_utf8(int str);
 
 #endif
