c3756c3d2b147191167b9b26561a7d72021512bc
simdax <simoncornaz@gmail.com>
2018-03-17 18:35:52 +0100

    add numbers

---
+++ numbers/numbers.h
@@ -0,0 +1,13 @@
+#ifndef NUMBERS_H
+# define NUMBERS_H
+
+int			ft_max(int one, int two);
+int			ft_min(int one, int two);
+unsigned int		ft_fact(unsigned int nb);
+int			ft_atoi(const char *str);
+float			ft_atof(const char *str);
+char			*ft_itoa(int n);
+char			**ft_all_perms(char *s);
+int			ft_abs(int value);
+
+#endif
